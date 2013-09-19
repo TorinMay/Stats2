1caaefbce0287b1967f9738ae7f71461b8866c9b
TorinMay <mayte@s.dcsdk12.org>
2013-09-19 08:43:33 -0600

    Initial Commit

---
+++ Stats.java
@@ -0,0 +1,13 @@
+public class Stats {
+	
+	public static void main(String[] args) {
+		int [] a ={1,2,3,4,5,6}
+	}
+
+	public static int max(int a) {
+		int max = a[0];
+		for (int i=0; i < a.length; i++) {
+			
+		}
+	}
+}
