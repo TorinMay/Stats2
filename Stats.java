992d21e325ca5b280fbcecdeece58b2e8cc6ec9b
TorinMay <mayte@s.dcsdk12.org>
2013-09-19 09:33:25 -0600

    Finished Median

--- Stats.java
+++ Stats.java
@@ -2,9 +2,13 @@ public class Stats {
 	
 	public static void main(String[] args) {
 		int[] a ={1,2,3,4,5,6};
+		int[] b ={1,2,3,4,5,6,7}; //Only used to test median
+	System.out.println("Here are the numbers:\n");
+	System.out.println("------------------------");
 	System.out.println("The max is " + max(a));
 	System.out.println("The min is " + min(a));
 	System.out.println("The mean is " + mean(a));
+	System.out.println("The median is " + median(b));
 
 	}
 
@@ -37,8 +41,28 @@ public class Stats {
 		return (sum / a.length);
 	}
 
-	public static int median(int [] a) {
-		
+	public static double median(int[] b) {
+		double median = 0.0;
+		for (int i=0; i < b.length; i++) {
+			median = b.length/2;
+		}
+		return median;
 	}
+
+	// public static quartile1(int[] a) {
+
+	// }
+
+	// public static quartile3(int[] a) {
+
+	// }
+
+	// public static mode(int[] a) {
+
+	// }
+
+	// public static standardeviation(int[] a) {
+
+	// }
 	
 }
